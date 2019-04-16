54480d58be3d09010108f57a7f0d11e2bbe08d99
Mike Clarke <mike@lambdafunctions.com>
2019-04-16 23:46:50 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use aho_corasick::AhoCorasick;
+
+use std::io::{Read, stdin, stdout};
+use std::process;
+use std::str;
+
+fn aho () {
+    let mut rdr = stdin();
+    let mut isa_buf = vec![0u8; 106];
+
+    match rdr.read_exact(&mut isa_buf) {
+        Ok(_) => {}
+        Err(ref e) if e.kind() == std::io::ErrorKind::UnexpectedEof => {
+            eprintln!("ISA segment is too short.");
+            process::exit(1);
+        }
+        Err(err) => {
+            eprintln!("Error processing stream: {}", err);
+            process::exit(1);
+        }
+
+    }
+
+    let terminator = str::from_utf8(&isa_buf[105..106]).unwrap();
+    let replacement = format!("{}\n", terminator);
+    let patterns = &[terminator];
+    let replace_with = &[replacement];
+
+    let ac = AhoCorasick::new(patterns);
+
+    match ac.stream_replace_all(rdr, &mut stdout(), replace_with) {
+        Ok(_) => {}
+        Err(ref e) if e.kind() == std::io::ErrorKind::BrokenPipe => {
+            process::exit(1);
+        }
+        Err(err) => {
+            eprintln!("Error processing stream: {}", err);
+            process::exit(1);
+        }
+    }
+}
+
+fn main() {
+    aho();
+}
